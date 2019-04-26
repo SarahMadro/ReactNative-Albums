9d2c315d91639d3f900bfa8d0f5b21c300345e86
Sarah <sjmadro1@gmail.com>
2019-04-26 12:04:06 -0700

    first commit initializing project and creating initial components in header and album list

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import library to help create a component
+import React from 'react';
+import { Text, AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+// Creata a component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList / >
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
